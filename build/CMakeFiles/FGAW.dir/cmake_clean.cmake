0c0c63e49725ced9ffa20f98c093de213c721f93
deud07 <joshuahomandub@gmail.com>
2022-04-02 12:49:19 +0100

    init

---
+++ build/CMakeFiles/FGAW.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FGAW.dir/src/FGAW.cpp.obj"
+  "CMakeFiles/FGAW.dir/src/FGAW.cpp.obj.d"
+  "FGAW.exe"
+  "FGAW.exe.manifest"
+  "FGAW.pdb"
+  "libFGAW.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FGAW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/proj/C++/FGAW
+# Build directory: D:/proj/C++/FGAW/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
